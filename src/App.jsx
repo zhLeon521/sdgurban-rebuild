ab5ce380672d6bfc7c9f68bfd4ecaa9ab02f734b
zhleon <zholeon@qq.com>
2022-04-28 21:21:06 +0800

    todo: header responsive

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SdgsDetails from './SdgsDetails';
+import Header from './Header';
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <SdgsDetails />
+    </>
+
+
+
+
+  )
+}
+
+export default App;
---
+++ src/SdgsDetails.jsx
@@ -0,0 +1,280 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Logo from './logo.svg'
+
+function SdgsDetails() {
+
+  return (
+    <section className="container px-0 mx-auto md:py-20 md:p-10 md:px-0">
+      <section className="grid items-start grid-cols-1 gap-0 md:p-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
+
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+        <article className="p-3 duration-300 transform hover:-translate-y-1 hover:shadow-2xl group">
+          <a target="_blank" href="https://unsplash.com/@mehranhadad">
+            <div className="relative overflow-hidden max-h-125">
+              <img className="absolute" src="http://urbansdg.org.cn/home/sdgBack/C_SDG_inverted_WEB-01.png" alt="" />
+              <img className="relative duration-500 transform group-hover:opacity-0" src="http://urbansdg.org.cn/home/sdgFront/C-WEB-Goal-01.png" alt="" />
+            </div>
+          </a>
+          <div className="absolute p-3 duration-500 transform bg-gray-200 rounded-full opacity-0 top-10 right-10 group-hover:opacity-100">
+            <a target="_blank" href="https://unsplash.com/@mehranhadad">
+              <svg className="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="rgba(0,0,0,0.5)">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+              </svg>
+            </a>
+          </div>
+        </article>
+
+
+
+
+
+      </section>
+    </section>
+
+
+
+
+  )
+}
+
+export default SdgsDetails;
